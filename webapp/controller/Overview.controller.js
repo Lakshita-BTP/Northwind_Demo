862b9c3f951718fe68d628e561ca4f916888c6b9
Jhony Gautam <lakshita.flow@gmail.com>
2025-12-10 11:45:54 +0000

    05. Add a JSON Model to the Application for inputs

--- webapp/controller/Overview.controller.js
+++ webapp/controller/Overview.controller.js
@@ -1,10 +1,13 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
+], (Controller, JSONModel) => {
     "use strict";
 
     return Controller.extend("northwinddemo.controller.Overview", {
-        onInit() {
+        onInit: function () {
+            var oModel = new JSONModel();
+            this.getView().setModel(oModel, "input");
         }
     });
 });
