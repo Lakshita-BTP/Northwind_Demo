fcd6f4e3a34b2b7a240f77b6a005c353eb7a9b69
Jhony Gautam <lakshita.flow@gmail.com>
2025-12-12 04:37:59 +0000

    10. Updating Controller onSave

--- webapp/controller/Overview.controller.js
+++ webapp/controller/Overview.controller.js
@@ -1,13 +1,34 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/model/json/JSONModel"
-], (Controller, JSONModel) => {
+    "sap/ui/model/json/JSONModel",
+    "sap/m/MessageToast"
+], (Controller, JSONModel, MessageToast) => {
     "use strict";
 
     return Controller.extend("northwinddemo.controller.Overview", {
         onInit: function () {
             var oModel = new JSONModel();
             this.getView().setModel(oModel, "input");
+        },
+
+        onSave: function () {
+            var oModelData = this.getView().getModel("input").getData();
+            var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+
+            //if (oModelData.Discount === undefined) { oModelData.Discount = 0; }
+
+            this.byId("productTable").getBinding("items").create({
+                "ID": oModelData.ID,
+                "Name": oModelData.Name,
+                "Description": oModelData.Description,
+                "ReleaseDate": oModelData.ReleaseDate,
+                "DiscontinuedDate": oModelData.DiscontinuedDate,
+                "Rating": oModelData.Rating,
+                "Price": oModelData.Price
+            }).created().then(function () {
+                MessageToast.show(oResourceBundle.getText("productCreatedMessage"));
+            });
+
         }
     });
 });
